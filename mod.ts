cb3e578ce8fd46e5905993dcbb5b3e609422d29c
Osman KOÇ <72092169+Osman-Dev@users.noreply.github.com>
2021-04-11 14:52:11 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -37,18 +37,18 @@ slash.commands.all().then((e) => {
       },
       {
         name: "activity",
-        description: "Parti Baslat!",
+        description: "Start an Activity in a Voice Channel.",
         options: [
           {
             name: "channel",
             type: slash.SlashCommandOptionType.CHANNEL,
-            description: "Partiyi baslatacagin ses kanali.",
+            description: "Voice Channel to start activity in.",
             required: true,
           },
           {
             name: "activity",
             type: slash.SlashCommandOptionType.STRING,
-            description: "Parti turu.",
+            description: "Activity to start.",
             required: true,
             choices: Object.entries(ACTIVITIES).map((e) => ({
               name: e[1].name,
@@ -66,10 +66,10 @@ slash.handle("activity", (d) => {
   const channel = d.option<slash.InteractionChannel>("channel");
   const activity = ACTIVITIES[d.option<string>("activity")];
   if (!channel || !activity) {
-    return d.reply("Hatali komut.", { ephemeral: true });
+    return d.reply("Invalid interaction.", { ephemeral: true });
   }
   if (channel.type !== slash.ChannelTypes.GUILD_VOICE) {
-    return d.reply("Komutlar sadece ses kanalinda kullanilabilir.", {
+    return d.reply("Activities can only be started in Voice Channels.", {
       ephemeral: true,
     });
   }
@@ -84,20 +84,20 @@ slash.handle("activity", (d) => {
     })
     .then((inv) => {
       d.reply(
-        `[${activity.name} partisini ${channel.name} kanalinda baslatmak icin tiklayin.](<https://discord.gg/${inv.code}>)`
+        `[Click here to start ${activity.name} in ${channel.name}.](<https://discord.gg/${inv.code}>)`
       );
     })
     .catch((e) => {
       console.log("Failed", e);
-      d.reply("Komut Kullanılamadı.", { ephemeral: true });
+      d.reply("Failed to start Activity.", { ephemeral: true });
     });
 });
 
 slash.handle("invite", (d) => {
   d.reply(
-    `• [Botu Sunucuna Ekle.](<https://discord.com/api/oauth2/authorize?client_id=799338366642552872&permissions=1&scope=applications.commands%20bot>)\n` +
-      `• [Lynx Topunun Youtube Kanalı.](<https://www.youtube.com/channel/UCxgah4yZuFp7ZAZ-gO39gdA>)\n` +
-      `• [Sunucumuza Katıl.](<https://discord.gg/zVAFeDZdKz>)`,
+    `• [Click here to invite.](<https://discord.com/api/oauth2/authorize?client_id=819835984388030464&permissions=1&scope=applications.commands%20bot>)\n` +
+      `• [Check out Source Code.](<https://github.com/DjDeveloperr/ActivitiesBot>)\n` +
+      `• [Join our Discord.](<https://discord.gg/WVN2JF2FRv>)`,
     { ephemeral: true }
   );
 });
